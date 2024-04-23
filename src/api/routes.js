8679e89bdcd06ca7ac1f764796589573584f85e8
Luka Janketić <luka.janketic@gmail.com>
2024-04-23 17:17:58 +0200

    Update routes.js
    fix export default

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,18 +1,18 @@
-import axiosInstance from './axiosInstance';
+import axiosInstance from './index';
 
-const axiosRoutes = () => ({
-    getAllTodos() {
-      return axiosInstance.get('/api/todos');
-    },
-    getSingleTodo(id) {
-      return axiosInstance.get(`/api/todos/${id}`);
-    },
-    createTodo(todoData) {
-      return axiosInstance.post('/api/todos', todoData);
-    },
-    updateTodo(id, todoData) {
-      return axiosInstance.patch(`/api/todos/${id}`, todoData);
-    }
-});
+const axiosRoutes = {
+  getAllTodos() {
+    return axiosInstance.get('/api/todos');
+  },
+  getSingleTodo(id) {
+    return axiosInstance.get(`/api/todos/${id}`);
+  },
+  createTodo(todoData) {
+    return axiosInstance.post('/api/todos', todoData);
+  },
+  updateTodo(id, todoData) {
+    return axiosInstance.patch(`/api/todos/${id}`, todoData);
+  }
+};
 
 export default axiosRoutes;
