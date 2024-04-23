a4b5b860db35c36e35d02cb279b4d805b19b520f
Luka Janketić <luka.janketic@gmail.com>
2024-04-23 15:55:51 +0200

    irrelevant

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sample header
         </p>
       </header>
     </div>
