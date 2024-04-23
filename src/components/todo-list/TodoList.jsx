8c590accf4b41e7886ce778ffe483c45e64bdae2
Luka Janketić <luka.janketic@gmail.com>
2024-04-23 16:40:44 +0200

    acceptable state of affairs

--- src/components/todo-list/TodoList.jsx
+++ src/components/todo-list/TodoList.jsx
@@ -1,24 +1,44 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Table } from 'react-bootstrap';
+
+const placeholderData = [
+    
+];
 
 function TodoList() {
   return (
     <Container className="mt-5">
       <h1 className="text-center mb-4">ToDo App</h1>
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Description</th>
-            <th scope="col">Done</th>
-            <th scope="col">Created At</th>
-            <th scope="col">Updated At</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* Placeholder atm */}
-        </tbody>
-      </table>
+      <div className="table-container text-center" style={{ maxHeight: '200px', minHeight: '400px', overflow: 'auto' }}>
+        <Table striped bordered>
+          <thead className='sticky-top'>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Description</th>
+              <th scope="col">Done</th>
+              <th scope="col">Created At</th>
+              <th scope="col">Updated At</th>
+            </tr>
+          </thead>
+          <tbody>
+            {placeholderData.length === 0 ? (
+              <tr>
+                <td colSpan="5">No items to display</td>
+              </tr>
+            ) : (
+              placeholderData.map(todo => (
+                <tr key={todo.id}>
+                  <td>{todo.id}</td>
+                  <td>{todo.description}</td>
+                  <td>{todo.done ? 'Yes' : 'No'}</td>
+                  <td>{todo.createdAt}</td>
+                  <td>{todo.updatedAt}</td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </Table>
+      </div>
     </Container>
   );
 }
