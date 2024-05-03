37cbc8b7bdc5551d6a1dca5a8fbd4366d824ab3c
Luka Janketić <luka.janketic@gmail.com>
2024-05-03 13:43:06 +0200

    Create TodoContext to remove logic from App.js

---
+++ src/components/context/TodoContext.js
@@ -0,0 +1,44 @@
+import React, { createContext, useContext, useState, useEffect, useCallback } from 'react';
+import PropTypes from 'prop-types';
+import axiosRoutes from './api/routes';
+
+const TodoContext = createContext();
+
+export const useTodoContext = () => useContext(TodoContext);
+
+export const TodoProvider = ({ children }) => {
+  const [todos, setTodos] = useState([]);
+  const [sortOrder, setSortOrder] = useState('DESC');
+
+  const refreshTodoList = useCallback(async () => {
+    try {
+      const { data } = await axiosRoutes.getAllTodosSorted(sortOrder);
+      setTodos(data);
+    } catch (error) {
+      console.error('Error fetching todos:', error);
+    }
+  }, [sortOrder]);
+
+  const addTodo = async (todoData) => {
+    try {
+      await axiosRoutes.createTodo(todoData);
+      refreshTodoList();
+    } catch (error) {
+      console.error('Error adding a new todo:', error);
+    }
+  };
+
+  useEffect(() => {
+    refreshTodoList();
+  }, [sortOrder, refreshTodoList]);
+
+  return (
+    <TodoContext.Provider value={{ todos, sortOrder, setSortOrder, addTodo }}>
+      {children}
+    </TodoContext.Provider>
+  );
+};
+
+TodoProvider.propTypes = {
+  children: PropTypes.node.isRequired
+};
